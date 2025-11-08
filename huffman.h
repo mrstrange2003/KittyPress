368242bf4095a66e76855090a70b83d7b6434e20
J Dipayan Rao <raodipayan@gmail.com>
2025-11-08 22:41:17 +0530

    huffman.h v2

--- huffman.h
+++ huffman.h
@@ -7,14 +7,16 @@
 #include <fstream>
 #include <bitset>
 #include <memory>
+#include <cstdint>
 
+// Use unsigned char for full 0..255 byte support
 struct HuffmanNode {
-    char ch;
+    unsigned char ch;
     int freq;
     HuffmanNode *left;
     HuffmanNode *right;
 
-    HuffmanNode(char c, int f) : ch(c), freq(f), left(nullptr), right(nullptr) {}
+    HuffmanNode(unsigned char c, int f) : ch(c), freq(f), left(nullptr), right(nullptr) {}
 };
 
 // Comparator for priority queue
@@ -25,5 +27,9 @@ struct Compare {
 };
 
 // Function declarations
-void compressFile(const std::string &inputPath, const std::string &outputPath);
-void decompressFile(const std::string &inputPath, const std::string &outputPath);
+void compressFile(const std::string &inputPath, const std::string &outputPath); // writes KP02 with isCompressed = true
+void decompressFile(const std::string &inputPath, const std::string &outputPath); // handles KP01 & KP02
+
+// Helpers for storing raw files inside .kitty (KP02 with isCompressed = false)
+void storeRawFile(const std::string &inputPath, const std::string &outputPath);
+void restoreRawFile(std::ifstream &inStream, const std::string &outputPath);
