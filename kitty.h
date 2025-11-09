be1f722bc0db1aa422277b9b54566d4509786162
J Dipayan Rao <raodipayan@gmail.com>
2025-11-09 19:34:21 +0530

    kitty.h v3

--- kitty.h
+++ kitty.h
@@ -2,6 +2,6 @@
 #pragma once
 #include <string>
 
-// Current write signature (KP02). We still accept KP01 when reading.
-const std::string KITTY_MAGIC_V2 = "KP02";
 const std::string KITTY_MAGIC_V1 = "KP01";
+const std::string KITTY_MAGIC_V2 = "KP02";
+const std::string KITTY_MAGIC_V3 = "KP03"; 
