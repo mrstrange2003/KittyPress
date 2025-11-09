9d0f0691fc5b0d609ccbbc5365bf9ec49f191ea2
J Dipayan Rao <raodipayan@gmail.com>
2025-11-09 19:29:45 +0530

    main.cpp v3

--- main.cpp
+++ main.cpp
@@ -2,29 +2,16 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
-#include <algorithm>   
-#include <vector>      
 #include "huffman.h"
 
 using namespace std;
 
 void printUsage() {
-    cout << "\nKittyPress v2 " << endl;
-    cout << "A lightweight universal archiver (Huffman for text; store for media).\n" << endl;
+    cout << "\nKittyPress v3 " << endl;
+    cout << "A universal lossless archiver using LZ77 + Huffman compression.\n" << endl;
     cout << "Usage:" << endl;
     cout << "  kittypress compress <input> <output.kitty>" << endl;
     cout << "  kittypress decompress <input.kitty> <output>\n" << endl;
-}
-
-bool isCompressibleExtension(const string &ext) {
-    string e = ext;
-    transform(e.begin(), e.end(), e.begin(), ::tolower);
-    static const vector<string> compressExts = {
-        ".txt", ".log", ".csv", ".json", ".xml", ".html", ".md",
-        ".cpp", ".c", ".h", ".py", ".js", ".java", ".css", ".ts",
-        ".tex", ".srt", ".vtt", ".ass", ".cfg", ".ini", ".yml"
-    };
-    return find(compressExts.begin(), compressExts.end(), e) != compressExts.end();
 }
 
 int main(int argc, char* argv[]) {
@@ -46,16 +33,12 @@ int main(int argc, char* argv[]) {
 
     try {
         if (mode == "compress") {
-            string ext = filesystem::path(inputPath).extension().string();
             cout << "Compressing: " << inputPath << " → " << outputPath << endl;
-            if (isCompressibleExtension(ext)) {
-                cout << "Mode: Huffman compression (text/source-code)\n";
-                compressFile(inputPath, outputPath);
-            } else {
-                cout << "Mode: Storing raw bytes (no compression)\n";
-                storeRawFile(inputPath, outputPath);
-                cout << "Stored without compression.\n";
-            }
+            cout << "Mode: Universal compression (LZ77 + Huffman)\n";
+
+            // Always compress, regardless of file type
+            compressFile(inputPath, outputPath);
+
             cout << "Compression complete.\n";
         }
         else if (mode == "decompress") {
