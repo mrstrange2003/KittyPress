630c92b3f870d3030fc627883f0da5ce4fae8417
J Dipayan Rao <raodipayan@gmail.com>
2025-11-09 20:36:10 +0530

    archive.h v4

---
+++ archive.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct ArchiveInput {
+    std::string absPath;  // actual disk path
+    std::string relPath;  // path inside archive
+};
+
+void createArchive(const std::vector<std::string>& inputs,
+                   const std::string& outputArchive);
+
+void extractArchive(const std::string& archivePath,
+                    const std::string& outputFolder);
